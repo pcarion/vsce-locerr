e18f852efca59c2f73b4a1d7d08703f714a76128
Pierre Carion <pierre@rabb.it>
2019-05-17 16:02:23 -0700

    First usable version of the extension
    - counter file hard coded at ~/vscode-locerr-counter.txt
    - output log statement is also hard coded

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,81 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import * as path from 'path';
+import * as fs from 'fs';
+
+// returns true if file was actually created
+async function createFileIfDoesNotExist(fileName: string) {
+	return new Promise<boolean>((resolve, reject) => {
+		fs.access(fileName, fs.constants.F_OK, (err) => {
+			if (err) {
+				// file does not exist
+				fs.writeFile(fileName,  '0', 'utf8', (err) => {
+					if (err) {
+						console.log('File is not updatable:', fileName);
+						return reject(err);
+					}
+					return resolve(true);
+				});
+			} else {
+				// file exists, we're done
+				return resolve(false);
+			}
+		});
+	});
+
+}
+
+async function getAndIncrementCounter() {
+	const counterFileName = path.join(process.env['HOME'] || '.','vscode-locerr-counter.txt');
+
+	return new Promise<Number>(async (resolve, reject) => {
+		const wasCreated = await createFileIfDoesNotExist(counterFileName);
+		if (wasCreated) {
+			console.log('Counter file created:', counterFileName);
+		}
+
+		fs.access(counterFileName, fs.constants.W_OK, (err) => {
+			if (err) {
+				console.log('File is not writable:', counterFileName);
+				return reject(err);
+			}
+			fs.readFile(counterFileName, 'utf8', (err, data) => {
+				if (err) {
+					console.log('File is not readable:', counterFileName);
+					return reject(err);
+				}
+				const counter = parseInt(data, 10);
+				const newCounter = counter + 1;
+				const newData = `${newCounter}`;
+				fs.writeFile(counterFileName,  newData, 'utf8', (err) => {
+					if (err) {
+						console.log('File is not updatable:', counterFileName);
+						return reject(err);
+					}
+					return resolve(counter);
+				});
+			});
+		});
+	});
+}
+
+function replaceEditorSelection(text: string) {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		console.log('No editor found');
+		return;
+	}
+  const selections = editor.selections;
+
+  editor.edit((editBuilder) => {
+    selections.forEach((selection) => {
+      editBuilder.replace(selection, '');
+      editBuilder.insert(selection.active, text);
+    });
+  });
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,11 +88,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.locerr', async () => {
 		// The code you place here will be executed every time your command is executed
 
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			console.log('No open text editor');
+			return; // No open text editor
+		}
+		// The code you place here will be executed every time your command is executed
+		const counter = await getAndIncrementCounter();
+		const logStatement = "log.error({ err, location: "+counter+" }, 'error');";
+
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		// vscode.window.showInformationMessage('Hello World!:' + counter);
+		replaceEditorSelection(logStatement);
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Hello World from locerr!');
 	});
 
 	context.subscriptions.push(disposable);
